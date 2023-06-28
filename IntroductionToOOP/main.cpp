81a96f09a5247d802c64576755a9da2b9ae8b103
kate0602 <burm-ekaterina@yandex.ru>
2023-06-28 22:49:13 +0400

    Add project files.

---
+++ IntroductionToOOP/main.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+
+class Point 
+{
+	double x;
+	double y;
+
+public:
+};
+
+//#define STRUCT_POINT
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef STRUCT_POINT
+
+    int a;
+	Point A;
+
+	A.x = 2;
+	A.y = 3;
+
+	cout << A.x << "\t" << A.y << endl;
+	cout << sizeof(A) << endl;
+	
+	Point* pA = &A;
+    cout << pA->x << "\t" << pA->y << endl;
+
+#endif  // STRUCT_POINT
+} 
