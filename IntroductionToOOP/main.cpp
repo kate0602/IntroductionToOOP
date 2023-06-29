625fefffa62e81ec19b1a6263f951c5a2d709d96
kate0602 <burm-ekaterina@yandex.ru>
2023-06-29 21:05:45 +0400

    IntroductionToOOP

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include<iostream>
 using namespace std;
+#define delimiter "\n------------------------------------------------------------------------------\n"
 
 class Point
 {
@@ -45,9 +46,38 @@ public:
 		cout << "Constructor: \t\t" << this << endl;
 	}
 
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor: \t" << this << endl;
+
+	}
+
 	~Point()
 	{
 		cout << "Destructor: \t\t" << this << endl;
+	}
+
+
+	// operators
+	
+	Point& operator =(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment: \t" << this << endl;
+		return *this;
+	}
+
+
+	//Метод
+	double distance(const Point& other)const
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
 	}
 
 	//metod
@@ -58,8 +88,19 @@ public:
 };
 
 
+//Функция
+double distance(const Point& A, const Point& B)
+{
+	
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance;
+}
 
 //#define STRUCT_POINT
+//#define CONSTRACTOR_CHECK
+//#define DISTANCE_CHECK
 
 void main()
 {
@@ -80,17 +121,62 @@ void main()
     cout << pA->x << "\t" << pA->y << endl;
 
 #endif  // STRUCT_POINT
+#ifdef CONSTRACTOR_CHECK
+
+ // CONSTRACTOR_CHECK
 
 	Point A;
 	//A.set_x(2);
 	//A.set_y(3);
 	A.print();
-	Point B = 5;
+
+	Point B = 5; // Single-argument constructor
 	B.print();
 
 	Point C(2, 3);
 	C.print();
 
-	//cout << A.get_x() << "\t" << A.get_y() << endl;
+	Point D = C; // copy constructor
+	D.print();
 
+	Point E;  // copy assignment
+	E = D;
+
+
+	//cout << A.get_x() << "\t" << A.get_y() << endl;
+#endif
+
+#ifdef DISTANCE_CHECK
+
+
+
+	Point A(2, 3);
+	A.print();
+
+	Point B(7, 8);
+	B.print();
+	cout << delimiter << endl;
+	cout << "Растояние от точки А до точки В:      " << A.distance(B) << endl;
+	cout << delimiter << endl;
+	cout << "Растояние от точки B до точки A:      " << B.distance(A) << endl;
+	cout << delimiter << endl;
+	cout << "Растояние между точками А до точки В: " << distance(A,B) << endl;
+	cout << delimiter << endl;
+	cout << "Растояние между точками B до точки A: " << distance(A,B) << endl;
+	cout << delimiter << endl;
+
+#endif // DISTANCE_CHECK
+
+
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << "\t" << b << "\t" << c << endl;
+
+	Point A, B, C;
+	cout << delimiter << endl;
+	A = B = C = Point(2, 3);
+	cout << delimiter << endl;
+	A.print();
+	B.print();
+	C.print();
 } 
